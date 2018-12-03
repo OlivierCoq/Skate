d8b253d114961e7013a685940225673a61ea76c9
Olivier Coq <coq.olivier36@gmail.com>
2018-12-02 20:12:06 -0500

    UX-UI

--- dist/js/production.js
+++ dist/js/production.js
@@ -1489,16 +1489,26 @@ $(document).ready(function(){
       // Add smooth scrolling to specific links
      
  
-        $('a.scroll').on('click', function(ev) {
-          ev.preventDefault();
-           
-               var target = this.hash,
-                   $target = $(target);
-           
-               $('html, body').animate({
-                   'scrollTop': $target.offset().top/* -70 //minus height of the navbar*/
-               }, 900); 
-           });
+        $('a').on('click', function(event) {
+            
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+                
+                // Store hash
+                var hash = this.hash;
+                
+                // Using jQuery's animate() method to add smooth page scroll
+                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 900, function() {
+                    
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                });
+                return false;
+            } // End if
+        }); 
 
         /*Scroll to Top Button*/
     $(window).scroll(function(){
