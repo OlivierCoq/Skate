28122252daa84346ff65a252e5ef659b3e7b1b5b
Olivier Coq <coq.olivier36@gmail.com>
2018-12-02 19:13:03 -0500

    UX-UI

--- dist/js/production.js
+++ dist/js/production.js
@@ -1488,20 +1488,22 @@ $(document).ready(function(){
     /*Scrolling*/
       // Add smooth scrolling to specific links
      
- if ($('a').parent().hasClass('scroll')) {
-     
-     $('a').on('click', function(ev) {
-       ev.preventDefault();
+    if ($('a').parent().hasClass('scroll')) {
         
-            var target = this.hash,
-                $target = $(target);
-        
-            $('html, body').animate({
-                'scrollTop': $target.offset().top/* -70 //minus height of the navbar*/
-            }, 900); 
-        });
-        
- }  
+        $(this).on('click', function(ev) {
+          ev.preventDefault();
+           
+               var target = this.hash,
+                   $target = $(target);
+           
+               $('html, body').animate({
+                   'scrollTop': $target.offset().top/* -70 //minus height of the navbar*/
+               }, 900); 
+           });
+           
+    } else {
+        $(this).unbind('click');
+    } 
         /*Scroll to Top Button*/
     $(window).scroll(function(){
         if ($(window).scrollTop() > 450) {
@@ -1510,9 +1512,7 @@ $(document).ready(function(){
              $("#scrollToTop").hide();
         } 
     }); 
-
-    
-});    
+});     
 $(function(){
     
     $.validator.addMethod('nameLength', function(value,element) {
--- library/js/app.js
+++ library/js/app.js
@@ -4,20 +4,22 @@ $(document).ready(function(){
     /*Scrolling*/
       // Add smooth scrolling to specific links
      
- if ($('a').parent().hasClass('scroll')) {
-     
-     $('a').on('click', function(ev) {
-       ev.preventDefault();
+    if ($('a').parent().hasClass('scroll')) {
         
-            var target = this.hash,
-                $target = $(target);
-        
-            $('html, body').animate({
-                'scrollTop': $target.offset().top/* -70 //minus height of the navbar*/
-            }, 900); 
-        });
-        
- }  
+        $(this).on('click', function(ev) {
+          ev.preventDefault();
+           
+               var target = this.hash,
+                   $target = $(target);
+           
+               $('html, body').animate({
+                   'scrollTop': $target.offset().top/* -70 //minus height of the navbar*/
+               }, 900); 
+           });
+           
+    } else {
+        $(this).unbind('click');
+    } 
         /*Scroll to Top Button*/
     $(window).scroll(function(){
         if ($(window).scrollTop() > 450) {
@@ -26,4 +28,4 @@ $(document).ready(function(){
              $("#scrollToTop").hide();
         } 
     }); 
-});    
+});     
